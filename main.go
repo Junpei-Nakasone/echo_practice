0ca7a7deeccfe3700b84802cfcf09191a223b529
Junpei-Nakasone <nakasone.junpei0719@gmail.com>
2019-11-30 22:43:31 +0900

    add func

--- main.go
+++ main.go
@@ -11,11 +11,34 @@ func hello(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello from echo.\n")
 }
 
+func getCats(c echo.Context) error {
+	catName := c.QueryParam("name")
+	catType := c.QueryParam("type")
+
+	dataType := c.Param("data")
+
+	if dataType == "string" {
+		return c.String(http.StatusOK, fmt.Sprintf("your cat name is : %s\nand his type is: %s\n", catName, catType))
+	}
+
+	if dataType == "json" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"name": catName,
+			"type": catType,
+		})
+	}
+
+	return c.JSON(http.StatusBadRequest, map[string]string{
+		"error": "you should specify the dataType.",
+	})
+}
+
 func main() {
 	fmt.Println("hello world from web server.")
 
 	e := echo.New()
 
 	e.GET("/", hello)
+	e.GET("/cats/:data", getCats)
 	e.Start(":8080")
 }
